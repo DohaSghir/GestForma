bdc56302758775132e8c1bb4b3cfe78f61718d2b
HayatBounouara <hayatbounouara20@gmail.com>
2024-12-31 20:41:20 +0100

    commit2

---
+++ GestForma/Services/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
+using GestForma.Models;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace GestForma.Services
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            var administrateur = new IdentityRole("administrateur");
+            administrateur.NormalizedName = "administrateur";
+
+
+            var professeur = new IdentityRole("professeur");
+            professeur.NormalizedName = "professeur";
+
+            var participant = new IdentityRole("participant");
+            participant.NormalizedName = "participant";
+
+            var invité = new IdentityRole("invité");
+            invité.NormalizedName = "invité";
+
+            builder.Entity<IdentityRole>().HasData(administrateur, professeur, participant, invité);
+
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+        }
+    }
+}
--- GestForma/obj/Debug/net9.0/GestForma.MvcApplicationPartsAssemblyInfo.cs
+++ GestForma/obj/Debug/net9.0/GestForma.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("GestForma")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee99e71b3f93a4db50e10a0f854964adaceb8ba2")]
-[assembly: System.Reflection.AssemblyProductAttribute("GestForma")]
-[assembly: System.Reflection.AssemblyTitleAttribute("GestForma")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
