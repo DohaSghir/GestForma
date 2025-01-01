9550ecdb882023cbce52a183a55b3ff53fc8e3f7
Doha Sghir <doha.sghir@usmba.ac.ma>
2025-01-01 17:01:51 +0100

    run

--- GestForma/obj/Debug/net9.0/GestForma.AssemblyInfo.cs
+++ GestForma/obj/Debug/net9.0/GestForma.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f52f71fe0a662948b7b4e53caa252e97dcefe414")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a160f9721e6abc651fff48a1fec2c00f8ae3d224")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
