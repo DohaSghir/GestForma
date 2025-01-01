777b9b93f48744085c74cbba4ec26c3042cec9f2
hibasekkah <157165724+hibasekkah@users.noreply.github.com>
2025-01-01 21:05:22 +0100

    n

--- GestForma/obj/Release/net9.0/GestForma.AssemblyInfo.cs
+++ GestForma/obj/Release/net9.0/GestForma.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d69e9ea673536adb9c119f111f53564fe9c0c8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f79057ac173f3c6d477c8e80211a67b1f148533a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
