8cd4dba2b7afbc084f8b82d100fdaa48143c604c
Manal-armel <manalarmel@gmail.com>
2025-01-02 16:44:52 +0100

    test changement profile

--- GestForma/obj/Debug/net9.0/GestForma.AssemblyInfo.cs
+++ GestForma/obj/Debug/net9.0/GestForma.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d50642eb1227715dcfa759a4a473e8a167cb3d43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aab5e34c93f251733fe89964c1305402e2bb3c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
