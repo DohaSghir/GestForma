da0565bcc00b54658efccf1cac2183d00410c992
Manal-armel <manalarmel@gmail.com>
2025-01-03 17:49:49 +0100

    commit

--- GestForma/obj/Debug/net9.0/GestForma.AssemblyInfo.cs
+++ GestForma/obj/Debug/net9.0/GestForma.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd4dba2b7afbc084f8b82d100fdaa48143c604c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83fb9317dfc197a14b86a985ce6c375458330c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
