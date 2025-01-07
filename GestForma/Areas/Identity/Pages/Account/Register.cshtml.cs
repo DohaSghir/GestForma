0b5a2ad4ff068bfc5c49aa3268dc226355ed976f
Manal-armel <manalarmel@gmail.com>
2025-01-07 20:05:53 +0100

    commit

--- GestForma/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ GestForma/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -166,7 +166,7 @@ namespace GestForma.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    TempData["SuccessMessage"] = "Registration successful! You will be redirected to the homepage shortly.";
+                   
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
