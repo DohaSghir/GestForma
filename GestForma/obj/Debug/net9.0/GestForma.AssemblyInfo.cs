5994c37f692f0f5d66b7aee08fe666b0683bc41b
hibasekkah <157165724+hibasekkah@users.noreply.github.com>
2025-01-07 09:17:46 +0000

    n

--- GestForma/obj/Debug/net9.0/GestForma.AssemblyInfo.cs
+++ GestForma/obj/Debug/net9.0/GestForma.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53104e0a4eb2fa2f9ea70c5fcdb808cbb8cea69d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+344f5a66d7366aacac7066d5bdad6001c4d48841")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestForma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
